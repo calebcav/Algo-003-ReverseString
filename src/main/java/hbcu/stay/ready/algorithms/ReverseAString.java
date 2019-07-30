f7bdffba6ef02881aa3ff225276f02c55024d325
Caleb <cavinesscaleb@gmail.com>
2019-07-30 13:21:07 -0400

    Done

--- src/main/java/hbcu/stay/ready/algorithms/ReverseAString.java
+++ src/main/java/hbcu/stay/ready/algorithms/ReverseAString.java
@@ -3,7 +3,36 @@ package hbcu.stay.ready.algorithms;
 public class ReverseAString {
 
     public String reverseAString(String input){
-        return null;
+
+        int x = 0;
+        int y = input.length() - 1;
+
+        while(x != y){
+
+            char firstLetter = input.charAt(x);
+            char secondLetter = input.charAt(y);
+
+            input = input.substring(0,x) + Character.toLowerCase(secondLetter) + input.substring(x+1);
+            input = input.substring(0,y) + Character.toLowerCase(firstLetter) + input.substring(y+1);
+
+            x++;
+            y--;
+        }
+
+
+
+
+        return capatalizeString(input);
+    }
+
+    private static String capatalizeString(String input){
+        input = input.substring(0,1).toUpperCase() + input.substring(1);
+        for (int i = 1; i < input.length(); i++){
+            if(input.charAt(i) == ' '){
+                input = input.substring(0,i) + " " + input.substring(i+1, i+2).toUpperCase() + input.substring(i+2);
+            }
+        }
+        return input;
     }
 
 
